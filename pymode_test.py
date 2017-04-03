96b8f87001f0d4a0e9a5ba5a24bb473c6983bc13
Ankit Gupta <ankit6791@gmail.com>
2017-04-03 13:09:17 -0700

    module in a subdir

--- pymode_test.py
+++ pymode_test.py
@@ -1,2 +1,14 @@
+import os, sys, inspect
 import pymod
+ # realpath() will make your script run, even if you symlink it :)i
+cmd_folder = os.path.realpath(os.path.abspath(os.path.split(inspect.getfile( inspect.currentframe() ))[0]))
+print(cmd_folder)
+if cmd_folder not in sys.path:
+    sys.path.insert(0, cmd_folder)
+
+ # use this if you want to include modules from a subfolder
+cmd_subfolder = os.path.realpath(os.path.abspath(os.path.join(os.path.split(inspect.getfile( inspect.currentframe() ))[0],"subfolder")))
+if cmd_subfolder not in sys.path:
+    sys.path.insert(0, cmd_subfolder)
+
 pymod.printit()
